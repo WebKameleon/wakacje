6068bdacecc57658ea8e6612bb4d9dcdb9194dec
Piotr Podstawski <piotr@webkameleon.com>
2015-08-25 17:50:30 +0000

    łukasz błąd

--- controllers/holidaysController.php
+++ controllers/holidaysController.php
@@ -89,7 +89,7 @@ class holidaysController extends merlinController {
         if (in_array($m+0,[1,3,5,7,8,10,12])) return '31';
         if (in_array($m+0,[4,6,9,11])) return '30';
         
-        $year=$date('Y');
+        $year=date('Y');
         if (date('m')>2) $year++;
         if ($year%4==0) return '29';
         return '28';
