f53a0f5e6c636384feb7930af41ae6d516abecef
Piotr Podstawski <piotr@webkameleon.com>
2015-10-08 11:15:44 +0000

    social

--- index.php
+++ index.php
@@ -135,5 +135,5 @@
 </body>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<script src="<?php echo $root;?>/resources/home.js"></script>
+<script src="<?php echo $root;?>resources/home.js"></script>
 </html>
